81b61fb355d4bfb319305c36a962b5db8257cb2d
29071 <lovestudy@qq.com>
2024-04-20 15:46:48 +0800

    0.2

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
-use iced::{Element, Sandbox, Settings};
-use iced::widget::text;
+use iced::{alignment, Element, Length, Sandbox, Settings};
+use iced::widget::{container, text};
 struct GroceryList {}
 #[derive(Debug, Clone)]
 enum Message {}
 impl Sandbox for GroceryList {
 	type Message = Message;
 	
-	/* Initialize your app */
+	/* 初始化应用 */
 	fn new() -> GroceryList {
 		Self {}
 	}
 	
 	/**
-	* The title of the window. It will show up on the top of your application window.
+	* 窗口的标题。它将显示在应用程序窗口的顶部。
 	*/
 	fn title(&self) -> String {
 		String::from("Grocery List App")
 	}
 	
 	fn update(&mut self, message: Self::Message) {
-		// Update the state of your app
+		// 更新应用信息状态
 	}
 	
 	fn view(&self) -> Element<Self::Message> {
-		text("This is where you will show the view of your app")
-		.into()
+        container(
+            text("Hello World")
+        )
+        .height(Length::Fill)
+        .width(Length::Fill)
+        .align_x(alignment::Horizontal::Center)
+        .align_y(alignment::Vertical::Center)
+        .into()
+    }
+    fn theme(&self) -> iced::Theme {
+		iced::Theme::Dark
 	}
 }
 fn main() -> iced::Result {
